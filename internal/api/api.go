c59293b55e3af8507ac54aa40b020f267ae99fae
Tamer_Li <tamer@li-work08.ru>
2024-12-07 02:18:47 +0300

    Rename files

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,1 +1,11 @@
 package api
+
+import "github.com/gorilla/mux"
+
+func New() *API {
+	api := API{
+		router: mux.NewRouter(),
+	}
+
+	return &api
+}
--- internal/api/types.go
+++ internal/api/types.go
@@ -5,11 +5,3 @@ import "github.com/gorilla/mux"
 type API struct {
 	router *mux.Router
 }
-
-func New() *API {
-	api := API{
-		router: mux.NewRouter(),
-	}
-
-	return &api
-}
