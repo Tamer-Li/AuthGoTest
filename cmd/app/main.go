c52790e122e344dbbf1d874c62b18bf997375a09
Tamer_Li <tamer@li-work08.ru>
2024-12-07 02:40:38 +0300

    Create API

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"AuthGoTest/internal/api"
 	"AuthGoTest/internal/config"
 	"flag"
 	"log"
+	"net/http"
 )
 
 func main() {
 	envStringPath := envFile()
 
 	cfg := config.MustLoad(envStringPath)
+
+	_ = cfg
+
+	srv := api.New()
+
+	http.ListenAndServe("localhost:8080", srv.Router())
 }
 
 func envFile() string {
--- internal/api/api.go
+++ internal/api/api.go
@@ -1,11 +1,29 @@
 package api
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func New() *API {
 	api := API{
 		router: mux.NewRouter(),
 	}
 
+	api.router.HandleFunc("/", Hello).Methods(http.MethodGet)
+
 	return &api
+}
+
+func (api *API) Router() *mux.Router {
+	return api.router
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, err := fmt.Fprintf(w, "<html><body><h2>Hello World</h2></body></html>")
+	if err != nil {
+		panic(err)
+	}
 }
